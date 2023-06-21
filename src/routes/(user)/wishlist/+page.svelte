aed3c883ca58a3c042205a9538246a25427defe5
Kyrell <kleanosiauw@student.ciputra.ac.id>
2023-06-22 01:27:07 +0700

    feat(wishlist): wishlist design stable

---
+++ src/routes/(user)/wishlist/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import {goto} from "$app/navigation";
+
+    let products = [
+        'Product 1',
+        'Product 2',
+        'Product 3',
+        'Product 4',
+        'Product 5',
+        'Product 6',
+        'Product 7',
+        'Product 8',
+    ];
+</script>
+
+<svelte:head>
+    <title>Wishlist | OpenMerce</title>
+</svelte:head>
+
+<div class="space-y-8">
+    <h4 class="font-extrabold text-2xl">Wishlist</h4>
+    <div class="grid grid-cols-1 xxs:grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-4">
+        {#each products as product}
+            <div
+                    class="rounded-md shadow-md cursor-pointer"
+                    on:click={() => goto(`/products/${product}`)}
+            >
+                <img src="/emptycart.png" alt="pic" class="aspect-square object-contain"/>
+
+                <div class="p-3">
+                    {product}
+                </div>
+            </div>
+        {/each}
+    </div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
-        extend: {}
+        extend: {},
+        screens: {
+            'xxs': '320px',
+            'xs': '400px',
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+        }
     },
     plugins: []
 };
